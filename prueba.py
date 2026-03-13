a67c642601602831d777e8f4cd44934857deac0e
hugobonforte <hugobonforte@gmail.com>
2026-03-12 20:28:04 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,1 +1,11 @@
+meme_dict = {"CRINGE": "Significa algo excepcionalmente raro o embarazoso",
+            "LOL": "Significa una respuesta común a algo gracioso",}
 
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+
+else:
+    print("Esa palabra no esta en el diccionario de memes")
+            
